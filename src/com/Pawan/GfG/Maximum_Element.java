3f01d05263b6fefbba16d70ceaf3ad5be862baf9
Pawan <paletipawankumar@gmail.com>
2023-06-23 14:40:14 +0530

    Adding the soln of the maximum elements in the array using the recursion

--- src/com/Pawan/GfG/Maximum_Element.java
+++ src/com/Pawan/GfG/Maximum_Element.java
@@ -1,9 +1,8 @@
 package com.Pawan.GfG;
 
-public class MaxAndMin {
+public class Maximum_Element {
     public static void main(String[] args) {
-
-        /*
+    /*
         Question link :- https://www.geeksforgeeks.org/recursive-programs-to-find-minimum-and-maximum-elements-of-array/
         Input: arr = {1, 4, 3, -5, -4, 8, 6};
         Output: min = -5, max = 8
@@ -12,18 +11,19 @@ public class MaxAndMin {
         Output: min = -8, max = 45
          */
 
-        int[] arr = {1, 4, 45, 6, 10, -8};
-        int lenght = arr.length;
-        int ans = maxMin(arr,arr.length);
-        System.out.println("the min element in the array is : "+ ans);
+        int[] nums = {1, 4, 3, -5, -4, 8, 6};
+
+        int ans = maxElement(nums,0);
+        System.out.println("The maximum element in the array is : " + ans);
+
     }
-    static int maxMin(int[] arr , int n){
+    static int maxElement(int[] arr , int n){
 //        base condition
-        if (n==1){
-            return arr[0];
+        if (n==6){
+            return arr[6];
         }
 
-        return Math.min(arr[n-1],maxMin(arr,n-1));
+//        recursive function
+        return Math.max(arr[n],maxElement(arr,n+1));
     }
-
 }
