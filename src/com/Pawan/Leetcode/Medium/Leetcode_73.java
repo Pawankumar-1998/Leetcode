1e7ecdf5a15808eed33cda0721037199da9c668b
Pawan <paletipawankumar@gmail.com>
2023-07-13 21:33:17 +0530

    adding sol leetcode 73

---
+++ src/com/Pawan/Leetcode/Medium/Leetcode_73.java
@@ -0,0 +1,11 @@
+package com.Pawan.Leetcode.Medium;
+
+public class Leetcode_73 {
+    public static void main(String[] args) {
+
+    }
+
+    static void setZero(int[][] matrix){
+        
+    }
+}
