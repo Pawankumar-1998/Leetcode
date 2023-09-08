a3fa4f528e075f68b681c2bb389f558712a98ce9
Pawan <paletipawankumar@gmail.com>
2023-09-08 13:39:00 +0530

    modifying the code in leetcode 169 with all test case passes ( brute force )

--- src/com/Pawan/Leetcode/BruteForce/Leetcode_169.java
+++ src/com/Pawan/Leetcode/BruteForce/Leetcode_169.java
@@ -2,27 +2,54 @@ package com.Pawan.Leetcode.BruteForce;
 
 public class Leetcode_169 {
     public static void main(String[] args) {
-        int[] arr = {6,5,5};
+        int[] arr = {6,6,6,7,7};
         System.out.println(majorityElement(arr));
     }
     static int majorityElement(int[] arr){
+
+        insertion(arr);
+        int arrayLength = arr.length;
+
+
+        int tempCount =0;
+        int highestNumber = 0;
+
         int ans = arr[0];
-        int greater = arr.length / 2;
-        int tempCount = 0;
 
-        for (int i = 0; i < arr.length-1; i++) {
-                int count = 0;
-            for (int j = 0; j < arr.length ; j++) {
-                if (arr[i] == arr[j]){
-                    count++;
+
+            for (int i = 0; i < arrayLength-1; i++) {
+                while (i<arrayLength-1 && arr[i] ==arr[i+1]){
+                    tempCount++;
+                    i++;
                 }
 
-                if (count>greater){
+                if (tempCount>highestNumber){
                     ans = arr[i];
+                    highestNumber = tempCount;
+                    tempCount = 0;
                 }
-            }
-            tempCount = count;
+
+
+
         }
         return ans;
+    }
+
+    static void insertion(int[] arr){
+        for (int i = 0; i < arr.length-1 ; i++) {
+            for (int j = i + 1; j > 0 ; j--) {
+                if (arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    static void swap(int[] arr , int first , int last){
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
     }
 }
